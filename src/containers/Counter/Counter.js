8724570cebb20028be1d68e5573f4b7c62afe7d6
Ibrahim shamma <i.abushammah@gmail.com>
2020-01-16 00:10:08 +0200

    commit done

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -39,19 +39,19 @@ class Counter extends Component {
         <CounterOutput value={this.props.ctr} />{' '}
         <CounterControl
           label="Increment"
-          clicked={() => this.counterChangedHandler('inc')}
+          clicked={this.props.onIncrementCounter}
         />{' '}
         <CounterControl
           label="Decrement"
-          clicked={() => this.counterChangedHandler('dec')}
+          clicked={this.props.onDecrementCounter}
         />{' '}
         <CounterControl
           label="Add 5"
-          clicked={() => this.counterChangedHandler('add', 5)}
+          clicked={this.props.onADDCounter}
         />{' '}
         <CounterControl
           label="Subtract 5"
-          clicked={() => this.counterChangedHandler('sub', 5)}
+          clicked={this.props.onSUBCounter}
         />{' '}
       </div>
     );
@@ -64,4 +64,14 @@ const mapStateToProps = state => {
     };
 };
 
-export default connect(mapStateToProps)(Counter);
+const mapDispatchToProps = dispatch =>{
+    return {
+      onIncrementCounter:() => dispatch({type:"INCREMENT"}),
+      onDecrementCounter:() => dispatch({type:"DECREMENT"}),
+      onADDCounter:() => dispatch({type:"ADD",value:5}),
+      onSUBCounter:() => dispatch({type:"SUB",value:5})
+
+
+    }
+};
+export default connect(mapStateToProps,mapDispatchToProps)(Counter);
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -4,8 +4,39 @@ const intialState = {
 
 
 const reducer = (state = intialState, action) => {
+    if (action.type === "INCREMENT") {
+        return {
+            ...state,
+            counter: state.counter + 1
+        };
+    }
+    if (action.type === "DECREMENT") {
+        return {
+            ...state,
+            counter: state.counter - 1
+        };
+
+    };
+
+    if (action.type === "ADD") {
+        return {
+            ...state,
+            counter: state.counter + action.value
+        };
+
+    };
+
+    if (action.type === "SUB") {
+        return {
+            ...state,
+            counter: state.counter - action.value
+        };
+
+    };
+
+
     return state
 };
 
 
-export default reducer
+export default reducer;
