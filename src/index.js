b4eaa2b1a3dd7ba476d15a0326c3520c72d6b93c
Ibrahim shamma <i.abushammah@gmail.com>
2020-01-15 14:14:15 +0200

    added redux

--- src/index.js
+++ src/index.js
@@ -4,9 +4,14 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import reducer from './store/reducer';
 const store = createStore(reducer);
 
-ReactDOM.render( < App / > , document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+     < App / > 
+     </Provider>
+     , document.getElementById('root'));
 registerServiceWorker();
