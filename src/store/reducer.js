b4575e26ba287f64bfabb08bb0a603ce2fe9d1e3
Ibrahim shamma <i.abushammah@gmail.com>
2020-01-16 09:57:03 +0200

    update

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -4,38 +4,32 @@ const intialState = {
 
 
 const reducer = (state = intialState, action) => {
-    if (action.type === "INCREMENT") {
-        return {
-            ...state,
-            counter: state.counter + 1
-        };
+
+    switch (action.type) {
+        case "INCREMENT":
+            return {
+                ...state,
+                counter: state.counter + 1
+            }
+        case "DECREMENT":
+            return {
+                ...state,
+                counter: state.counter - 1
+            };
+
+        case "ADD":
+            return {
+                ...state,
+                counter: state.counter + action.value
+            };
+        case "SUB":
+            return {
+                ...state,
+                counter: state.counter - action.value
+            };
     }
-    if (action.type === "DECREMENT") {
-        return {
-            ...state,
-            counter: state.counter - 1
-        };
 
-    };
-
-    if (action.type === "ADD") {
-        return {
-            ...state,
-            counter: state.counter + action.value
-        };
-
-    };
-
-    if (action.type === "SUB") {
-        return {
-            ...state,
-            counter: state.counter - action.value
-        };
-
-    };
-
-
-    return state
+    return state;
 };
 
 
